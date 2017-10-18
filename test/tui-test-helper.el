54d1fdaf4ab5725a9808bf1db05f7701b7ca2dd4
Erik Anderson <erikbpanderson@gmail.com>
2017-10-18 10:35:32 -0500

    Disable undercover

--- test/tui-test-helper.el
+++ test/tui-test-helper.el
@@ -1,7 +1,7 @@
 ;;; test-helper.el --- tui.el: Unit-test setup -*- lexical-binding: t; -*-
 
-(when (require 'undercover nil t)
-  (undercover "*.el"))
+;; (when (require 'undercover nil t)
+;;   (undercover "*.el"))
 
 (provide 'tui-test-helper)
 

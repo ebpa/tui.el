aa62a42a68c88767bf7eb023842e211acea298c9
Erik Anderson <erikbpanderson@gmail.com>
2017-12-06 23:16:11 -0600

    Added tui-toggle-debug-log

--- tui-log.el
+++ tui-log.el
@@ -5,8 +5,18 @@
 
 ;;; Code:
 
-(setq log-warning-minimum-level :debug)
-(setq warning-minimum-level :debug)
+(setq log-warning-minimum-level :warning)
+(setq warning-minimum-level :warning)
+
+(defun tui-toggle-debug-log ()
+  "Switch logging level between :warning<->:debug"
+  (interactive)
+  (if (eq log-warning-minimum-level :debug)
+      (progn
+        (setq log-warning-minimum-level :warning)
+        (setq warning-minimum-level :warning))
+    (setq log-warning-minimum-level :debug)
+    (setq warning-minimum-level :debug)))
 
 (defvar tui-log-buffer-name "*Tui Log*")
 

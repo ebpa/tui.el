7bded185d4abaad261c5533b036f936b1534b107
Ahmad Jarara <git@ajarara.io>
2023-09-04 21:48:35 -0400

    Consolidate inference

--- tui-hooks-x.el
+++ tui-hooks-x.el
@@ -2,18 +2,18 @@
 
 (require 'tui-hooks)
 
+(defmacro tui-hooks-x--infer-dependencies (body)
+  `(list ,@(cl-loop for sym in (flatten-tree body)
+                    collect `(ignore-errors ,sym))))
+
 (defmacro tui-use-inferred-effect (component &rest effect)
   (declare (indent defun))
-  (let ((dependencies
-         `(list ,@(cl-loop for sym in (flatten-tree effect)
-                           collect `(ignore-errors ,sym)))))
+  (let ((dependencies `(tui-hooks-x--infer-dependencies ,effect)))
      `(tui-use-effect ,component ,dependencies (lambda () ,@effect))))
 
 (defmacro tui-use-inferred-memo (component &rest compute)
   (declare (indent defun))
-  (let ((dependencies
-         `(list ,@(cl-loop for sym in (flatten-tree compute)
-                           collect `(ignore-errors ,sym)))))
-     `(tui-use-memo ,component ,dependencies (lambda () ,@compute))))
+  (let ((dependencies `(tui-hooks-x--infer-dependencies ,compute)))
+    `(tui-use-memo ,component ,dependencies (lambda () ,@compute))))
 
 (defalias 'tui-use-inferred-callback 'tui-use-inferred-memo)
